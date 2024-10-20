2b35431996a96750deb4557f36a08ff8c4c043fb
Marcin Baraniecki <marcin.baraniecki@softwaremill.com>
2024-10-20 21:44:26 +0200

    async/await on client

--- src/http/client.ts
+++ src/http/client.ts
@@ -4,4 +4,7 @@ const API_ROOT = 'https://jsonplaceholder.typicode.com'
 
 // 1. Promise<any> - why is it a good idea? why Promise<unknown> would be better in that case?
 // 2. what is 'unknown' type? how is it different from 'any'?
-export const getTodos = () => axios.get(`${API_ROOT}/todos`).then((r) => r.data)
+export const getTodos = async () => {
+  const { data } = await axios.get(`${API_ROOT}/todos`)
+  return data
+}
